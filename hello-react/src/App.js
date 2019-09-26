302800541c76390611f5533d20565e029f615c24
정윤도 <truenorthj@gmail.com>
2019-09-27 02:40:29 +0900

    react base

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MyComponent from "./MyComponent";
+import Counter from "./Counter"
+import Say from "./Say";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    const name = "리액트";
+    return (
+    <fragment>
+      <div className="react">{name}</div>
+      <MyComponent favoriteNumber={1}>칠드런 리엑트</MyComponent>
+      <Counter></Counter>
+      <Say></Say>
+    </fragment>);
+  }
+
+  
 }
 
 export default App;
--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import MyComponent from './MyComponent';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
