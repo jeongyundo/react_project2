538a3b73e0d8db082a520bb78d4eccd13c3508ef
정윤도 <truenorthj@gmail.com>
2019-09-27 17:41:58 +0900

    added Iteration Sample

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,20 +1,31 @@
-import React, {Component} from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import MyComponent from "./MyComponent";
-import Counter from "./Counter"
-import Say from "./Say";
-import EventPractice from "./EventPractice";
-import ValidationSample from "./ValidationSample";
+import React, { Component } from "react";
+import LifeCycleSample from "./LifeCycleSample";
+import ErrorBoundary from "./ErrorBoundary";
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
-  render(){
-    return (
-       <ValidationSample></ValidationSample>
-    )
-  }
+  state = {
+    color: "#000000"
+  };
 
-  
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  };
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
+    );
+  }
 }
 
 export default App;
