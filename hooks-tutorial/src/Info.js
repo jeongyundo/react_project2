bbd93ff00faf067e24e27368906467e334d038a2
정윤도 <truenorthj@gmail.com>
2019-09-28 03:24:19 +0900

    added info js

---
+++ hooks-tutorial/src/Info.js
@@ -0,0 +1,50 @@
+import React, {useState, useEffect} from 'react'
+import useInputs from './useInputs'
+
+const Info = () => {
+
+    const [state, onChange] = useInputs({
+        name: '',
+        nickname: ''
+    })
+    const { name, nickname} = state;
+    
+    // const [name, setName] = useState('');
+    // const [nickname, setNickname] = useState('');
+
+    // useEffect(() => {
+    //     console.log('effect');
+    //     console.log(name);
+    //     return () => {
+    //         console.log('cleanup');
+    //         console.log(name)
+    //     };
+    // });
+
+    // const onChangeName = e => {
+    //     setName(e.target.value);
+    // };
+
+    // const onChangeNickname = e => {
+    //     setNickname(e.target.value);
+    // };
+
+    return (
+        <div>
+            <div>
+                <input value={name} onChange={onChangeName} />
+                <input value={nickname} onChange={onChangeNickname} />
+            </div>
+            <div>
+                <div>
+                    <b>이름: </b>{name}
+                </div>
+                <div>
+                    <b>닉네임: </b>{nickname}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Info
