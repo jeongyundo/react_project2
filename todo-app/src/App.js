d6fb92d296b58df9e1c7b44c4e38a0432d61ad01
정윤도 <truenorthj@gmail.com>
2019-09-30 23:59:34 +0900

    todoapp updated

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,13 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
+import TodoTemplate from './components/TodoTemplate';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        Todo App을 만들자!
-      </div>
-    );
-  }
+const App = () => {
+  return <TodoTemplate>Todo App을 만들자!</TodoTemplate>
 }
 
 export default App;
